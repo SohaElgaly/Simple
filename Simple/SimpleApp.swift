b3ef284ba8b2ec516198528d0dd98179df77081b
Soha <sohaelgalyy@gmail.com>
2026-01-29 15:28:26 +0200

    Initial Commit

---
+++ Simple/SimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleApp.swift
+//  Simple
+//
+//  Created by Soha Elgaly on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
